5b3d4ae7855465c0a6f17d6d09783bbc936352c0
sa527 <lilin010110@gmail.com>
2023-08-13 16:11:05 +0930

    minishell.c v2.6

--- minishell.c
+++ minishell.c
@@ -110,8 +110,8 @@ int main(int argk, char *argv[], char *envp[])
 			default: /* code executed only by parent process */
 			{
 				if (background) {
-					//static int bgCounter = 1; // Initialize once and increment for each new background process
-                	printf("[%d] pid\n", frkRtnVal);
+					static int bgCounter = 1; // Initialize once and increment for each new background process
+                	printf("[%d] pid\n", bgCounter++);
 				} else {
 					wpid = wait(0);
 					if (wpid == -1) {
